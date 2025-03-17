85d7f26754ed0009c10f88b6a7eeedbce315a9b6
motasimdev <themotasimdev@gmail.com>
2025-03-17 11:05:27 +0600

    aa

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('.service_slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
