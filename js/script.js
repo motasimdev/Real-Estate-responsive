2ddc56e0c72704c96441df155b993f841c299741
motasimdev <themotasimdev@gmail.com>
2025-03-17 11:10:32 +0600

    aa

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ $('.service_slide').slick({
     },
   ]
 });
+
+  //  Stykey Header  
+  $(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+    
+    if(scrolling > 200){
+        $('.navbar').addClass('my_stick_bg');
+    }
+    else {
+         $('.navbar').removeClass('my_stick_bg');
+    }
+    });  
